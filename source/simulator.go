e06f87b0494c65915ffa4933a9d223baa2cb6ccd
Gábor Major <61381495+NotYetTerminal@users.noreply.github.com>
2024-11-05 08:43:55 +0000

    Added dimension swapping and started chunking

--- source/simulator.go
+++ source/simulator.go
@@ -12,19 +12,77 @@ import (
 	"sync"
 )
 
-// Swimming animal struct
+// Swimming animal struct used for fish and sharks
 type swimmingAnimal struct {
-	// Boolean to store whether this is a fish or shark
-	isFish bool
+	// Boolean to store whether this is a shark or fish
+	isShark bool
 	// Counter since last reproduction time
 	reproductionTime int
 	// Energy counter for starvation
 	energy int
+	// Used to see if a swimmingAnimal has been checked this iteration
+	checkingState bool
 }
 
-// Creates swimmingAnimals
-func newSwimmingAnimal(isFish bool, reproductionTime, energy int) *swimmingAnimal {
-	return &swimmingAnimal{isFish: isFish, reproductionTime: reproductionTime, energy: energy}
+// Constructor
+func newSwimmingAnimal(isShark bool, reproductionTime, energy int) *swimmingAnimal {
+	return &swimmingAnimal{isShark: isShark, reproductionTime: reproductionTime, energy: energy}
+}
+
+//func (sA *swimmingAnimal) process() {
+//	var potentialLocations []int
+//
+//}
+
+// Primitive data struct to construct bigger chunks
+type dataChunk struct {
+	data [][]*swimmingAnimal
+	// Pointers to surrounding chunks
+	leftChunk   *dataChunk
+	rightChunk  *dataChunk
+	topChunk    *dataChunk
+	bottomChunk *dataChunk
+}
+
+// Chunk border is used for passing data between threads
+type chunkBorder struct {
+	afterDataChunk  dataChunk
+	beforeDataChunk dataChunk
+	lock            sync.Mutex
+}
+
+// Constructor
+func newChunkBorder(topThreadDataChunk, bottomThreadDataChunk *dataChunk) *chunkBorder {
+	afterDataChunk := dataChunk{} // NEED DATA
+	beforeDataChunk := dataChunk{}
+
+	afterDataChunk.leftChunk = &afterDataChunk
+	afterDataChunk.rightChunk = &afterDataChunk
+	afterDataChunk.topChunk = topThreadDataChunk
+	afterDataChunk.bottomChunk = &beforeDataChunk
+
+	beforeDataChunk.leftChunk = &beforeDataChunk
+	beforeDataChunk.rightChunk = &beforeDataChunk
+	beforeDataChunk.topChunk = &afterDataChunk
+	beforeDataChunk.bottomChunk = bottomThreadDataChunk
+
+	return &chunkBorder{beforeDataChunk: beforeDataChunk, lock: sync.Mutex{}, afterDataChunk: afterDataChunk}
+}
+
+// Process the dataBefore array
+func (cB *chunkBorder) processDataBefore(checkingShark bool) {
+	cB.lock.Lock()
+	//for index := range cB.dataBefore {
+	//	swimmingAnimal := cB.dataBefore[index]
+	//	if swimmingAnimal != nil {
+	//		if checkingShark && swimmingAnimal.isShark {
+	//			swimmingAnimal.process()
+	//		} else if !checkingShark && !swimmingAnimal.isShark {
+	//
+	//		}
+	//	}
+	//}
+	cB.lock.Unlock()
 }
 
 // Handles taking in an integer from the user
@@ -52,12 +110,16 @@ func indexToPosition(index, sizeX int) (posX, posY int) {
 }
 
 // Create chunks of the worldGrid along the X axis
-func createXChunk(xIndex, chunkSizeX int, worldGrid [][]*swimmingAnimal) [][]*swimmingAnimal {
-	var newSlice [][]*swimmingAnimal
-	for index := range worldGrid {
-		newSlice = append(newSlice, worldGrid[index][chunkSizeX*xIndex:(chunkSizeX*xIndex)+chunkSizeX])
-	}
-	return newSlice
+//func createXChunk(xIndex, chunkSizeX int, worldGrid [][]*swimmingAnimal) [][]*swimmingAnimal {
+//	var newSlice [][]*swimmingAnimal
+//	for index := range worldGrid {
+//		newSlice = append(newSlice, worldGrid[index][chunkSizeX*xIndex:(chunkSizeX*xIndex)+chunkSizeX])
+//	}
+//	return newSlice
+//}
+
+func stepTime(chunk [][]*swimmingAnimal, beforeBorder []*swimmingAnimal, afterBorder []*swimmingAnimal) {
+
 }
 
 func main() {
@@ -65,6 +127,8 @@ func main() {
 	random := rand.New(rand.NewPCG(42, 1024))
 
 	// Declare variables
+	//globalCheckingState := false
+
 	var NumShark int
 	var NumFish int
 	var FishBreed int
@@ -115,14 +179,14 @@ func main() {
 
 	// Spawn sharks on grid
 	for index := range NumShark {
-		newShark := newSwimmingAnimal(false, SharkBreed, Starve)
+		newShark := newSwimmingAnimal(true, SharkBreed, Starve)
 		posX, posY := indexToPosition(positionsSlice[index], GridSizeX)
 		worldGrid[posY][posX] = newShark
 	}
 
 	// Spawn fish on grid
 	for index := range NumFish {
-		newFish := newSwimmingAnimal(true, FishBreed, 0)
+		newFish := newSwimmingAnimal(false, FishBreed, 0)
 		posX, posY := indexToPosition(positionsSlice[index+NumShark], GridSizeX)
 		worldGrid[posY][posX] = newFish
 	}
@@ -131,32 +195,45 @@ func main() {
 	// Split world into chunks for threads
 	// Each chunks 2 edges are duplicated
 	var chunksSlice [][][]*swimmingAnimal
+	swapped := false
 	if GridSizeX > GridSizeY {
-		chunkSizeX := GridSizeX / Threads
-		// Get equal sizes
-		for xIndex := range Threads - 1 {
-			var newSlice [][]*swimmingAnimal
-			for index := range worldGrid {
-				newSlice = append(newSlice, worldGrid[index][chunkSizeX*xIndex:(chunkSizeX*xIndex)+chunkSizeX])
-			}
-			chunksSlice = append(chunksSlice, newSlice)
-		}
-		var newSlice [][]*swimmingAnimal
-		// Get last one which is a little bigger
-		for index := range worldGrid {
-			newSlice = append(newSlice, worldGrid[index][chunkSizeX*(Threads-1):])
-		}
-		chunksSlice = append(chunksSlice, newSlice)
-	} else {
-		chunkSizeY := GridSizeY / Threads
-		// Get equal sizes
-		for index := range Threads - 1 {
-			chunksSlice = append(chunksSlice, worldGrid[chunkSizeY*index:(chunkSizeY*index)+chunkSizeY])
-		}
-		// Get last one which is a little bigger
-		chunksSlice = append(chunksSlice, worldGrid[chunkSizeY*(Threads-1):])
+		GridSizeY, GridSizeX = GridSizeX, GridSizeY
+		swapped = true
+		//chunkSizeX := GridSizeX / Threads
+		//// Get equal sizes
+		//for xIndex := range Threads - 1 {
+		//	var newSlice [][]*swimmingAnimal
+		//	for index := range worldGrid {
+		//		newSlice = append(newSlice, worldGrid[index][chunkSizeX*xIndex:(chunkSizeX*xIndex)+chunkSizeX])
+		//	}
+		//	chunksSlice = append(chunksSlice, newSlice)
+		//}
+		//var newSlice [][]*swimmingAnimal
+		//// Get last one which is a little bigger
+		//for index := range worldGrid {
+		//	newSlice = append(newSlice, worldGrid[index][chunkSizeX*(Threads-1):])
+		//}
+		//chunksSlice = append(chunksSlice, newSlice)
 	}
+	//} else {
+	//	chunkSizeY := GridSizeY / Threads
+	//	// Get equal sizes
+	//	for index := range Threads - 1 {
+	//		chunksSlice = append(chunksSlice, worldGrid[chunkSizeY*index:(chunkSizeY*index)+chunkSizeY])
+	//	}
+	//	// Get last one which is a little bigger
+	//	chunksSlice = append(chunksSlice, worldGrid[chunkSizeY*(Threads-1):])
+	//}
+
+	chunkSizeY := GridSizeY / Threads
+	// Get equal sizes
+	for index := range Threads - 1 {
+		chunksSlice = append(chunksSlice, worldGrid[chunkSizeY*index:(chunkSizeY*index)+chunkSizeY])
+	}
+	// Get last one which is a little bigger
+	chunksSlice = append(chunksSlice, worldGrid[chunkSizeY*(Threads-1):])
 	fmt.Println(chunksSlice)
+	ft.println(swapped)
 
 	// Create Atomic chunk border arrays
 	if Threads > 1 {
